ebcb33bf1c81e65b75a1bfe812c5ccd20511b56d
MatheusFP18 <matheusfelippe15r@gmail.com>
2025-09-04 22:59:05 -0300

    Add images

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,11 +11,9 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }));
 
-// Lógica para as abas da página de produtos
 const tabButtons = document.querySelectorAll(".tab-button[role='tab']");
 const tabPanels = document.querySelectorAll(".tab-panel[role='tabpanel']");
 
-// Executa o código apenas se os botões de aba existirem na página atual
 if (tabButtons.length > 0 && tabPanels.length > 0) {
     tabButtons.forEach(button => {
         button.addEventListener("click", () => {
@@ -23,7 +21,6 @@ if (tabButtons.length > 0 && tabPanels.length > 0) {
             const targetPanelId = button.getAttribute("aria-controls");
             const targetPanel = document.getElementById(targetPanelId);
 
-            // Remove a classe 'active' e 'aria-selected' de todos
             tabButtons.forEach(btn => {
                 btn.classList.remove("active");
                 btn.setAttribute("aria-selected", "false");
@@ -32,7 +29,6 @@ if (tabButtons.length > 0 && tabPanels.length > 0) {
                 panel.classList.remove("active");
             });
 
-            // Adiciona a classe 'active' e 'aria-selected' ao item clicado
             button.classList.add("active");
             button.setAttribute("aria-selected", "true");
             targetPanel.classList.add("active");
